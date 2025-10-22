82ec8c80bef7e90739be2cb75a7273645e2fcdd8
Abd-Allah-Djaber <jaber39986@gmail.com>
2025-10-22 14:19:18 +0200

    TP1_EX1.cpp

--- TP1_EX1.cpp
+++ TP1_EX1.cpp
@@ -1,27 +1,46 @@
 #include <stdio.h>
-
+void ScanMat2d(int n, int A[][n]);
+void SwapTrngl2d(int n, int A[][n]);
+void PrtMat2d(int n, int A[][n]);
 int main() {
-    int n = 4;
-    int a[4][4] = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12},
-        {13, 14, 15, 16}
-    };
-
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            int temp = a[i][j];
-            a[i][j] = a[j][i];
-            a[j][i] = temp;
+    int n;
+    printf("Enter size of matrix: ");
+    scanf("%d", &n);
+    int A[n][n];
+    printf("Enter A[%d][%d]:\n",n,n);
+    ScanMat2d(n, A);
+    printf("\nMatrix before swapping:\n");
+    PrtMat2d(n, A);
+    SwapTrngl2d(n, A);
+    printf("\nMatrix after swapping:\n");
+    PrtMat2d(n, A);
+    return 0;
+}
+//
+ void SwapTrngl2d(int n, int A[][n]) {
+    int i, j, temp;
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            temp = A[i][j];
+            A[i][j] = A[j][i];
+            A[j][i] = temp;
         }
     }
-
-    printf("\tMatrix after symmetry:\n\n\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++)
-            printf("\t%d", a[i][j]);
-        printf("\n\n\n");
+}
+//
+void PrtMat2d(int n, int A[][n]){
+    int i, j;
+    for (i = 0; i < n; i++){
+        printf("\n\n");
+        for (j = 0; j < n; j++)
+            printf("\t%d", A[i][j]);
+        printf("\n");
     }
-    return 0;
+}
+//
+void ScanMat2d(int n, int A[][n]){
+    int i, j;
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &A[i][j]);
 }
