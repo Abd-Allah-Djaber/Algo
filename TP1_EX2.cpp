d5f4e6d70b8cc06f6a5380bbf8956f6dfa062a94
Abd-Allah-Djaber <jaber39986@gmail.com>
2025-10-22 14:20:21 +0200

    TP1_EX2.cpp

--- TP1_EX2.cpp
+++ TP1_EX2.cpp
@@ -1,37 +1,34 @@
 #include <stdio.h>
-
+int add(int a, int b);
+int max(int a, int b);
+float average(int arr[], int n);
+int main() {
+    int n, i, j;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d numbers:\n", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    int largest = arr[0];
+    for (i = 1; i < n; i++)
+        largest = max(largest, arr[i]);
+    printf("Largest number = %d\n", largest);
+    printf("Average = %.2f\n", average(arr, n));
+    return 0;
+}
+//
 int add(int a, int b) {
     return a + b;
 }
-
+//
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
-
+//
 float average(int arr[], int n) {
-    int sum = 0;
-    for (int i = 0; i < n; i++)
-        sum += arr[i];
+    int i, sum = 0;
+    for (i = 0; i < n; i++)
+        sum = add(sum, arr[i]);
     return (float)sum / n;
-}
-
-int main() {
-    int n;
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
-
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        printf("arr[%d] = ", i);
-        scanf("%d", &arr[i]);
-    }
-
-    int maxVal = arr[0];
-    for (int i = 1; i < n; i++)
-        maxVal = max(maxVal, arr[i]);
-
-    printf("Maximum value = %d\n", maxVal);
-    printf("Average = %.2f\n", average(arr, n));
-
-    return 0;
 }
